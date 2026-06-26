b8b5f34ef056d859f1162aeec79df3858137fdc0
Ashish Raj Singh <ashishrajsingh75@gmail.com>
2026-06-26 15:39:51 +0530

    feat: enigo mouse movement implementation

--- src-tauri/src/input.rs
+++ src-tauri/src/input.rs
@@ -1,4 +1,4 @@
-use enigo::{Enigo, Key, KeyboardControllable};
+use enigo::{Enigo, Key, KeyboardControllable, MouseControllable};
 use log::{info, error};
 
 
@@ -6,16 +6,20 @@ pub fn execute_keypress(key_id: &str){
     info!("Key pressed:  {}", key_id);
     let mut enigo = Enigo::new();
     match key_id {
-        "ArrowLeft" => enigo.key_up(Key::LeftArrow),
+        "ArrowLeft" => enigo.key_down(Key::LeftArrow),
         "ArrowRight" => enigo.key_down(Key::RightArrow),
-        "ArrowUp" => enigo.key_up(Key::UpArrow),
+        "ArrowUp" => enigo.key_down(Key::UpArrow),
         "ArrowDown" => enigo.key_down(Key::DownArrow),
         _ => error!("Unknown key: {}", key_id)
     }
 }
 
 pub fn execute_trackpad_move(dx: f64, dy: f64){
+    let mut enigo = Enigo::new();
 
-    info!("Trackpad move:  {}, {}", dx, dy);
+    let sensitivity =  2.5;
+    let move_x = (dx * sensitivity).round() as i32;
+    let move_y = (dy * sensitivity).round() as i32;
 
+    enigo.mouse_move_relative(move_x, move_y);
 }
